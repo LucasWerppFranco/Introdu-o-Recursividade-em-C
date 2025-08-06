584c7a74e9ef7673e4fa3180e72ce7614b180a1c
Lucas Alves Antunes <llucas.alves0505@gmail.com>
2025-08-05 23:54:03 -0300

    Update Fibonacci.c

--- Recursividade/Fibonacci.c
+++ Recursividade/Fibonacci.c
@@ -1,17 +1,26 @@
 // Exercício 2: Solicite um número n e exiba os n primeiros termos da sequência de Fibonacci.
 #include <stdio.h>
 
+// Função recursiva de Fibonacci
 int fibonacci(int n) {
     if (n == 0) return 0;
     if (n == 1) return 1;
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
 int main() {
-    int i;
-    printf("Sequência de Fibonacci (10 primeiros):\n");
-    for (i = 0; i < 10; i++) {
+    int n, i;
+
+    // Entrada do usuário
+    printf("Digite quantos termos da sequência de Fibonacci deseja ver: ");
+    scanf("%d", &n);
+
+    // Impressão dos termos da sequência
+    printf("Sequência de Fibonacci (%d primeiros):\n", n);
+    for (i = 0; i < n; i++) {
         printf("%d ", fibonacci(i));
     }
     printf("\n");
+
     return 0;
 }
